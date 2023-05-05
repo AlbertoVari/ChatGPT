4d4a077ffeccf1863285040a886b2a46d247f90b
Alberto Varignana <22276126+AlbertoVari@users.noreply.github.com>
2023-05-05 13:22:00 +0200

    Add files via upload
    https://docs.tweepy.org/en/stable/examples.html

---
+++ create-tweet.py
@@ -0,0 +1,31 @@
+import tweepy
+api_key =""
+api_key_secret = ""
+access_token = ""
+access_token_secret = ""
+nearer_token =""
+consumer_key = api_key
+consumer_secret = api_key_secret
+clinet_id =""
+client_secret =""
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+# Create Tweet
+
+# The app and the corresponding credentials must have the Write permission
+
+# Check the App permissions section of the Settings tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+
+# Make sure to reauthorize your app / regenerate your access token and secret 
+# after setting the Write permission
+
+response = client.create_tweet(
+    text="This Tweet was Tweeted using Tweepy and Twitter API v2!"
+)
+print(f"https://twitter.com/user/status/{response.data['id']}")
